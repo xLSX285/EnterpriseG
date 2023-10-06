54329d8a3bfd3997a18e864217ce84dfc753acb0
Leon <129116755+xLSX285@users.noreply.github.com>
2023-10-06 12:17:55 +0200

    added compatibility checks, QOL improvements, moved more config to config.json & more

--- Build.ps1
+++ Build.ps1
@@ -19,6 +19,17 @@ if ($missingFiles) {
 $imageInfo = Get-WindowsImage -ImagePath "install.wim" -Index 1
 $Windows = ($imageInfo.ImageName -split ' ')[1]
 $Build = $imageInfo.Version
+$allowedSPBuilds = @(1, 1000, 1001)
+if ($imageInfo.SPBuild -notin $allowedSPBuilds) {
+    Write-Host "Mounted image contains updates. Updates should be added after reconstruction."
+    pause
+    exit 1
+}
+if ($imageInfo.ImageName -notlike "*Pro*") {
+    Write-Host "Mounted image does not appear to be the Pro Edition."
+    pause
+    exit 1
+}
 $detectBuildType = [int]($Build -replace '1[0-9]\.\d+\.', '')
 if ($detectBuildType -lt 19041) {
     $Type = "Legacy"
@@ -27,36 +38,32 @@ if ($detectBuildType -lt 19041) {
 } else {
     $Type = "vNext"
 }
-$WimToESD = "False"
-$RemoveApps = "False"
-$RemovePackages = "False"
-$DisableFeatures = "False"
-$ActivateWindows = "False"
-Write-Host "----------------------------------------------"
-Write-Output "Loading configuration"
-Write-Output "- Windows: $Windows"
-Write-Output "- Build: $Build"
-Write-Output "- Type: $Type"
-Write-Output "- WimToESD: $WimToESD"
-Write-Output "- RemoveApps: $RemoveApps"
-Write-Output "- RemovePackages: $RemovePackages"
-Write-Output "- DisableFeatures: $DisableFeatures"
-Write-Output "- ActivateWindows: $ActivateWindows"
-Write-Host "- Successfully loaded config.json"
+
 $config = (Get-Content "config.json" -Raw) | ConvertFrom-Json
+$ActivateWindows = $config.ActivateWindows
+$WimToESD = $config.WimToESD
+$RemoveApps = $config.RemoveApps
 $unwantedProvisionedPackages = $config.ProvisionedPackagesToRemove
+$AppCount = $unwantedProvisionedPackages.Count
+$RemovePackages = $config.RemovePackages
 $unwantedWindowsPackages = $config.WindowsPackagesToRemove
+$PackageCount = $unwantedWindowsPackages.Count
+$DisableFeatures = $config.DisableFeatures
 $unwantedWindowsFeatures = $config.WindowsFeaturesToDisable
-Write-Host ""
-Write-Output "Creating variables"
-$installImageFolder = "mount\"
-$SourcePath = "files\sxs\$Type\*.*"
-$DestinationPath = "sxs\"
+$FeatureCount = $unwantedWindowsFeatures.Count
 $yes = (cmd /c "choice <nul 2>nul")[1]
-Write-Host "- installImageFolder: $InstallImageFolder"
-Write-Host "- SourcePath: $SourcePath"
-Write-Host "- DestinationPath: $DestinationPath"
-Write-Host "----------------------------------------------"
+
+Write-Host ""
+Write-Host "Loading configuration"
+Write-Host "- Windows: $Windows"
+Write-Host "- Build: $Build"
+Write-Host "- Type: $Type"
+Write-Host "- ActivateWindows: $ActivateWindows"
+Write-Host "- WimToESD: $WimToESD"
+Write-Host "- RemoveApps: $RemoveApps" [$AppCount Apps detected]
+Write-Host "- RemovePackages: $RemovePackages" [$PackageCount Packages detected]
+Write-Host "- DisableFeatures: $DisableFeatures" [$FeatureCount Features detected]
+Write-Host ""
 Write-Host ""
 
 $folders = @("mount", "lp", "sxs")
@@ -66,24 +73,24 @@ foreach ($folder in $folders) {
     }
 }
 
-Write-Host "----------------------------------------------"
+Write-Host ""
 Write-Host "Extracting language pack & Edition files"
 Write-Host "- Microsoft-Windows-EditionSpecific-EnterpriseG-Package.ESD"
 .\files\7z.exe x Microsoft-Windows-EditionSpecific-EnterpriseG-Package.ESD -osxs | Out-Null
 Write-Host "- Microsoft-Windows-Client-LanguagePack-Package-amd64-en-us.esd"
 .\files\7z.exe x Microsoft-Windows-Client-LanguagePack-Package-amd64-en-us.esd -olp | Out-Null
-Write-Host "----------------------------------------------"
+Write-Host ""
 Write-Host ""
 
-Write-Host "----------------------------------------------"
+Write-Host ""
 Write-Host "Mounting Image"
 dism /mount-wim /wimfile:install.wim /index:1 /mountdir:mount | Out-Null
 Write-Host "- install.wim"
-Write-Host "----------------------------------------------"
+Write-Host ""
 Write-Host ""
 
 if ($Type -in "Normal", "vNext", "Legacy") {
-    Copy-Item -Path $SourcePath -Destination $DestinationPath -Force | Out-Null
+    Copy-Item -Path "files\sxs\$Type\*.*" -Destination "sxs\" -Force | Out-Null
 
     Rename-Item -Path "sxs\Microsoft-Windows-EnterpriseGEdition~31bf3856ad364e35~amd64~~10.0.22621.1.mum" -NewName "Microsoft-Windows-EnterpriseGEdition~31bf3856ad364e35~amd64~~$Build.mum" -Force | Out-Null
     Rename-Item -Path "sxs\Microsoft-Windows-EnterpriseGEdition~31bf3856ad364e35~amd64~~10.0.22621.1.cat" -NewName "Microsoft-Windows-EnterpriseGEdition~31bf3856ad364e35~amd64~~$Build.cat" -Force | Out-Null
@@ -92,38 +99,38 @@ if ($Type -in "Normal", "vNext", "Legacy") {
     (Get-Content "sxs\1.xml") -replace '10\.0\.22621\.1', $Build | Set-Content "sxs\1.xml" -Force | Out-Null
 }
 
-Write-Host "----------------------------------------------"
+Write-Host ""
 Write-Host "Converting SKU"
 dism /image:mount /apply-unattend:sxs\1.xml
-Write-Host "----------------------------------------------"
+Write-Host ""
 Write-Host ""
 
-Write-Host "----------------------------------------------"
+Write-Host ""
 Write-Host "Adding Language Pack"
 dism /image:mount /add-package:lp
-Write-Host "----------------------------------------------"
+Write-Host ""
 Write-Host ""
 Remove-Item -Path mount\Windows\*.xml -ErrorAction SilentlyContinue | Out-Null
 Copy-Item -Path mount\Windows\servicing\Editions\EnterpriseGEdition.xml -Destination mount\Windows\EnterpriseG.xml -ErrorAction SilentlyContinue | Out-Null
 
-Write-Host "----------------------------------------------"
+Write-Host ""
 Write-Host "Setting SKU to EnterpriseG"
 dism /image:mount /apply-unattend:mount\Windows\EnterpriseG.xml | Out-Null
 dism /image:mount /set-productkey:YYVX9-NTFWV-6MDM3-9PT4T-4M68B | Out-Null
 dism /image:mount /get-currentedition
-Write-Host "----------------------------------------------"
+Write-Host ""
 Write-Host ""
 
-Write-Host "----------------------------------------------"
+Write-Host ""
 Write-Host "Loading Registry Hive"
 reg load HKLM\zSOFTWARE mount\Windows\System32\config\SOFTWARE | Out-Null
 Write-Host "- zSOFTWARE"
 reg load HKLM\zSYSTEM mount\Windows\System32\config\SYSTEM | Out-Null
 Write-Host "- zSYSTEM"
-Write-Host "----------------------------------------------"
+Write-Host ""
 Write-Host ""
 
-Write-Host "----------------------------------------------"
+Write-Host ""
 Write-Host "Applying Registry Keys"
 
 # Add Microsoft Account support
@@ -141,123 +148,119 @@ reg add "HKLM\zSOFTWARE\Policies\Microsoft\MRT" /v "DontReportInfectionInformati
 reg add "HKLM\zSOFTWARE\Policies\Microsoft\Windows Defender\Signature Updates" /v "ForceUpdateFromMU" /t REG_DWORD /d "0" /f | Out-Null
 reg add "HKLM\zSOFTWARE\Policies\Microsoft\Windows Defender\Signature Updates" /v "UpdateOnStartUp" /t REG_DWORD /d "0" /f | Out-Null
 Write-Host "- Disable Defender Updates"
-Write-Host "----------------------------------------------"
+Write-Host ""
 Write-Host ""
 
-Write-Host "----------------------------------------------"
+Write-Host ""
 Write-Host "Unloading Registry Hive"
 reg unload HKLM\zSOFTWARE | Out-Null
 Write-Host "- zSOFTWARE"
 reg unload HKLM\zSYSTEM | Out-Null
 Write-Host "- zSYSTEM"
-Write-Host "----------------------------------------------"
+Write-Host ""
 Write-Host ""
 
-Write-Host "----------------------------------------------"
+Write-Host ""
 Write-Host "Adding License/EULA"
 mkdir mount\Windows\System32\Licenses\neutral\_Default\EnterpriseG -ErrorAction SilentlyContinue | Out-Null
 Write-Host "- New directory mount\Windows\System32\Licenses\neutral\_Default\EnterpriseG"
 Copy-Item -Path "files\License\license.rtf" -Destination "mount\Windows\System32\Licenses\neutral\_Default\EnterpriseG\license.rtf" -Force | Out-Null
 Write-Host "- license.rtf"
-Write-Host "----------------------------------------------"
+Write-Host ""
 Write-Host ""
 
 if ($ActivateWindows -eq "True") {
-	Write-Host "----------------------------------------------"
+	Write-Host ""
     Write-Host "Adding activation for Windows using KMS38"
     $null = New-Item -ItemType Directory -Path "mount\Windows\Setup\Scripts" -Force | Out-Null
     Copy-Item -Path "files\Scripts\MAS_AIO.cmd" -Destination "mount\Windows\Setup\Scripts\MAS_AIO.cmd" -Force | Out-Null
 	Write-Host "- MAS_AIO.cmd"
     Copy-Item -Path "files\Scripts\SetupComplete.cmd" -Destination "mount\Windows\Setup\Scripts\SetupComplete.cmd" -Force | Out-Null
 	Write-Host "- SetupComplete.cmd"
-	Write-Host "----------------------------------------------"
+	Write-Host ""
     Write-Host ""
 }
 
 if ($RemoveApps -eq "True") {
-    Write-Host "----------------------------------------------"
-	Write-Output "Removing inbox apps"
-	$detectedProvisionedPackages = Get-AppxProvisionedPackage -Path $installImageFolder
+    Write-Host ""
+	Write-Host "Removing inbox apps"
+	$detectedProvisionedPackages = Get-AppxProvisionedPackage -Path "mount\"
 
 	foreach ($detectedProvisionedPackage in $detectedProvisionedPackages) {
 		foreach ($unwantedProvisionedPackage in $unwantedProvisionedPackages) {
 			if ($detectedProvisionedPackage.PackageName.Contains($unwantedProvisionedPackage)) {
-				Remove-AppxProvisionedPackage -Path $installImageFolder -PackageName $detectedProvisionedPackage.PackageName -ErrorAction SilentlyContinue | Out-Null
+				Remove-AppxProvisionedPackage -Path "mount\" -PackageName $detectedProvisionedPackage.PackageName -ErrorAction SilentlyContinue | Out-Null
 			}
 		}
 	}
-    Write-Host "----------------------------------------------"
+    Write-Host ""
     Write-Host ""
 }
 
 if ($RemovePackages -eq "True") {
-    Write-Host "----------------------------------------------"
-	Write-Output "Removing packages"
-	$detectedWindowsPackages = Get-WindowsPackage -Path $installImageFolder
+    Write-Host ""
+	Write-Host "Removing packages"
+	$detectedWindowsPackages = Get-WindowsPackage -Path "mount\"
 
 	foreach ($detectedWindowsPackage in $detectedWindowsPackages) {
 		foreach ($unwantedWindowsPackage in $unwantedWindowsPackages) {
 			if ($detectedWindowsPackage.PackageName.Contains($unwantedWindowsPackage)) {
-				Remove-WindowsPackage -Path $installImageFolder -PackageName $detectedWindowsPackage.PackageName -ErrorAction SilentlyContinue | Out-Null
+				Remove-WindowsPackage -Path "mount\" -PackageName $detectedWindowsPackage.PackageName -ErrorAction SilentlyContinue | Out-Null
 			}
 		}
 	}
-    Write-Host "----------------------------------------------"
+    Write-Host ""
     Write-Host ""
 }
 
 if ($DisableFeatures -eq "True") {
-    Write-Host "----------------------------------------------"
-	Write-Output "Disabling features"
-	$detectedWindowsFeatures = Get-WindowsOptionalFeature -Path $installImageFolder
+    Write-Host ""
+	Write-Host "Disabling features"
+	$detectedWindowsFeatures = Get-WindowsOptionalFeature -Path "mount\"
 
 	foreach ($detectedWindowsFeature in $detectedWindowsFeatures) {
 		foreach ($unwantedWindowsFeature in $unwantedWindowsFeatures) {
 			if ($detectedWindowsFeature.FeatureName.Contains($unwantedWindowsFeature)) {
-				Disable-WindowsOptionalFeature -Path $installImageFolder -FeatureName $detectedWindowsFeature.FeatureName -ErrorAction SilentlyContinue | Out-Null
+				Disable-WindowsOptionalFeature -Path "mount\" -FeatureName $detectedWindowsFeature.FeatureName -ErrorAction SilentlyContinue | Out-Null
 			}
 		}
 	}
-    Write-Host "----------------------------------------------"
+    Write-Host ""
     Write-Host ""
 }
 
-Write-Host "----------------------------------------------"
+Write-Host ""
 Write-Host "Unmounting Install.wim Image"
 dism /unmount-wim /mountdir:mount /commit | Out-Null
 Write-Host "- install.wim"
 if ($LASTEXITCODE -ne 0) { exit 1 }
-Write-Host "----------------------------------------------"
+Write-Host ""
 Write-Host ""
 
-Write-Host "----------------------------------------------"
+Write-Host ""
 Write-Host "Optimizing Install.wim Image"
 & "files\wimlib-imagex" optimize install.wim
-Write-Host "----------------------------------------------"
+Write-Host ""
 Write-Host ""
 
-Write-Host "----------------------------------------------"
+Write-Host ""
 Write-Host "Setting WIM Infos"
 Write-Host ""
-& "files\wimlib-imagex" info install.wim 1 --image-property NAME="Windows $Windows EnterpriseG" --image-property DESCRIPTION="Windows $Windows EnterpriseG" --image-property FLAGS="EnterpriseG" --image-property DISPLAYNAME="Windows $Windows Enterprise G" --image-property DISPLAYDESCRIPTION="Windows $Windows Enterprise G" | Out-Null
-Write-Host "Name: Windows $Windows EnterpriseG"
-Write-Host "Description: Windows $Windows EnterpriseG"
-Write-Host "Flag: EnterpriseG"
-Write-Host "Display name: Windows $Windows EnterpriseG"
-Write-Host "Display description: Windows $Windows EnterpriseG"
-Write-Host "----------------------------------------------"
+& "files\wimlib-imagex" info install.wim 1 --image-property NAME="Windows $Windows EnterpriseG" --image-property DESCRIPTION="Windows $Windows EnterpriseG" --image-property FLAGS="EnterpriseG" --image-property DISPLAYNAME="Windows $Windows Enterprise G" --image-property DISPLAYDESCRIPTION="Windows $Windows Enterprise G"
+Write-Host ""
 Write-Host ""
 
 if ($WimToESD -eq "True") {
-    Write-Host "----------------------------------------------"
+    Write-Host ""
     Write-Host "Converting WIM to ESD"
     dism /Export-Image /SourceImageFile:install.wim /SourceIndex:1 /DestinationImageFile:install.esd /Compress:Recovery | Out-Null
     Write-Host "- Install.wim -> Install.esd"
     if (Test-Path "install.wim") { Remove-Item "install.wim" | Out-Null }
-    Write-Host "----------------------------------------------"
+    Write-Host ""
     Write-Host ""
 }
 
+timeout 5 | Out-Null
 $foldersToRemove = @("mount", "lp", "sxs")
 foreach ($folder in $foldersToRemove) {
     if (Test-Path $folder) {
@@ -268,9 +271,11 @@ Write-Host ""
 
 $endTime = Get-Date
 $elapsedTime = $endTime - $startTime
-Write-Host "----------------------------------------------"
-Write-Host "EnterpriseG completed in $($elapsedTime.TotalSeconds) seconds."
-Write-Host "----------------------------------------------"
+$elapsedMinutes = [math]::Floor($elapsedTime.TotalMinutes)
+$elapsedSeconds = $elapsedTime.Seconds
+Write-Host ""
+Write-Host "EnterpriseG completed in $($elapsedMinutes) minutes and $($elapsedSeconds) seconds."
+Write-Host ""
 Write-Host ""
 pause
 exit
